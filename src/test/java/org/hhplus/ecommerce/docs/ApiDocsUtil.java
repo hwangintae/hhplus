2409ff4433d797502d40138bf7f0d4f1a3cfa617
intaeHwang <hwang9029@ckinform.com>
2024-10-07 00:35:47 +0900

    git init

---
+++ src/test/java/org/hhplus/ecommerce/docs/ApiDocsUtil.java
@@ -0,0 +1,18 @@
+package org.hhplus.ecommerce.docs;
+
+import org.springframework.restdocs.operation.preprocess.OperationRequestPreprocessor;
+import org.springframework.restdocs.operation.preprocess.OperationResponsePreprocessor;
+
+import static org.springframework.restdocs.operation.preprocess.Preprocessors.*;
+import static org.springframework.restdocs.operation.preprocess.Preprocessors.prettyPrint;
+
+public interface ApiDocsUtil {
+
+    static OperationRequestPreprocessor getDocumentRequest() {
+        return preprocessRequest(prettyPrint());
+    }
+
+    static OperationResponsePreprocessor getDocumentResponse() {
+        return preprocessResponse(prettyPrint());
+    }
+}
